29a844c881a22cfc5ca808c395d40d32f7b23ccf
kjvtough <kjvtough@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-02-27 03:27:57 +0000

    Tagging for release 2.3
    git-svn-id: https://plugins.svn.wordpress.org/content-protector/tags/2.3@1100908 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/jquery-ui-datetime-i18n.php
@@ -0,0 +1,80 @@
+<?php
+global $wp_locale;
+
+function strip_array_indices( $ArrayToStrip ) {
+    foreach( $ArrayToStrip as $objArrayItem) {
+        $NewArray[] =  $objArrayItem;
+    }
+
+    return( $NewArray );
+}
+
+function date_format_php_to_js( $sFormat ) {
+    switch( $sFormat ) {
+        //Predefined WP date formats
+        case 'F j, Y':
+            return( 'MM dd, yy' );
+            break;
+        case 'Y/m/d':
+            return( 'yy/mm/dd' );
+            break;
+        case 'm/d/Y':
+            return( 'mm/dd/yy' );
+            break;
+        case 'd/m/Y':
+            return( 'dd/mm/yy' );
+            break;
+    }
+}
+
+if ( !isset( $jquery_ui_datetime_datepicker_i18n ) )
+    $jquery_ui_datetime_datepicker_i18n = array(
+        "closeText" => _x( "Done", "jQuery UI Datepicker Close label", "content-protector" ), // Display text for close link
+        "prevText" => _x( "Prev", "jQuery UI Datepicker Previous label", "content-protector" ), // Display text for previous month link
+        "nextText" => _x( "Next", "jQuery UI Datepicker Next label", "content-protector" ), // Display text for next month link
+        "currentText" => _x( "Today", "jQuery UI Datepicker Today label", "content-protector" ), // Display text for current month link
+        // we must replace the text indices for the following arrays with 0-based arrays
+        "monthNames" => strip_array_indices( $wp_locale->month ), // Names of months for drop-down and formatting
+        "monthNamesShort" => strip_array_indices( $wp_locale->month_abbrev ), // For formatting
+        "dayNames" => strip_array_indices( $wp_locale->weekday ), // For formatting
+        "dayNamesShort" => strip_array_indices( $wp_locale->weekday_abbrev ), // For formatting
+        "dayNamesMin" => strip_array_indices( $wp_locale->weekday_initial ), // Column headings for days starting at Sunday
+        "weekHeader" => _x( "Wk", "jQuery UI Datepicker Week label", "content-protector" ), // Column header for week of the year
+        "dateFormat" => date_format_php_to_js( get_option( 'date_format' ) ),
+        "firstDay" => get_option( 'start_of_week' ),
+        "isRTL" => $wp_locale->is_rtl(),
+        "showMonthAfterYear" => false, // True if the year select precedes month, false for month then year
+        "yearSuffix" => '' // Additional text to append to the year in the month headers
+     );
+$tf = get_option( 'time_format' );
+if ( false !== strpos( $tf, "A") ) {
+    $meridiem = array($wp_locale->meridiem['AM'], $wp_locale->meridiem['PM']);
+    $show_period = true;
+    $show_period_labels = true;
+    $show_leading_zero = false;
+} elseif ( false !== strpos( $tf, "a") ) {
+    $meridiem = array($wp_locale->meridiem['am'], $wp_locale->meridiem['pm']);
+    $show_period = true;
+    $show_period_labels = true;
+    $show_leading_zero = false;
+} else {
+    $meridiem = array('a', 'p');
+    $show_period = false;
+    $show_period_labels = false;
+    $show_leading_zero = true;
+}
+
+if ( !isset( $jquery_ui_datetime_timepicker_i18n ) )
+    $jquery_ui_datetime_timepicker_i18n = array(
+        "hourText" => _x( "Hour", "jQuery UI Timepicker 'Hour' label", "content-protector" ),
+        "minuteText" => _x( "Minute", "jQuery UI Timepicker 'Minute' label", "content-protector" ),
+        "amPmText" => $meridiem,
+        "showPeriod" => $show_period,
+        "showPeriodLabels" => $show_period_labels,
+        "showLeadingZero" => $show_leading_zero,
+        "timeSeparator" => _x( ":", "jQuery UI Datepicker: Character used to separate hours and minutes in translated language", 'content-protector' ),
+        "closeButtonText" => _x( "Done", "jQuery UI Timepicker 'Done' label", "content-protector" ),
+        "nowButtonText" => _x( "Now", "jQuery UI Timepicker 'Now' label", "content-protector" ),
+        "deselectButtonText" => _x( "Deselect", "jQuery UI Timepicker 'Deselect' label", "content-protector" ) );
+
+?>
